042e5d4e3614c274a5cdce66970cad479481ad27
Amanpreet Singh <amanpreet@codalien.com>
2018-05-08 11:46:41 +0530

    added customized module for country selector

--- app/screen/Onboard/CountrySelector/countrySelector.js
+++ app/screen/Onboard/CountrySelector/countrySelector.js
@@ -1,43 +1,180 @@
 import React, { Component } from 'react'
-import {View} from 'react-native'  
+import { Text, TouchableHighlight, TouchableWithoutFeedback, TouchableOpacity, View, TextInput, FlatList, Image } from 'react-native'
 import styles from './styles'
 import CountryPicker, {
     getAllCountries
 } from 'react-native-country-picker-modal';
+import Modal from "react-native-modal";
+import { FontAwesome } from '@expo/vector-icons'
+import cca2List from '../data/cca2'
+const countries = require('../data/countries')
+import Fuse from 'fuse.js'
+
+
+class MyListItem extends React.PureComponent {
+    _onPress = () => {
+        this.props.onPressItem(this.props.id);
+    };
+
+    render() {
+        const textColor = this.props.selected ? "red" : "black";
+        return (
+            <TouchableOpacity onPress={this._onPress}>
+                <View>
+                    <Text style={{ color: textColor }}>
+                        {this.props.title}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+
 
 class CountrySelector extends Component {
-
     constructor(props) {
         super(props);
-        let cca2, callingCode;
+        let cca2, callingCode, countryList = cca2List;
         cca2 = 'IN'
         callingCode = '+91'
 
         this.state = {
             cca2,
-            callingCode
+            callingCode,
+            modalVisible: false,
+            filter: '',
+            dataValue: cca2List
         }
+
+
+        this.fuse = new Fuse(
+            countryList.reduce(
+                (acc, item) => [
+                    ...acc,
+                    { id: item, name: this.getCountryName(countries[item]) }
+                ],
+                []
+            ),
+            {
+                shouldSort: true,
+                threshold: 0.6,
+                maxPatternLength: 32,
+                minMatchCharLength: 1,
+                keys: ['name'],
+                id: 'id'
+            }
+        )
+    }
+    toggleModal() {
+        this.setState({
+            modalVisible: !this.state.modalVisible
+        })
+    }
+    onSelectCountry(cca2) {
+        this.setState({
+            modalVisible: false,
+            filter: '',
+            dataValue: cca2List
+        })
     }
 
-    render() {
-        const ALL_COUNTRIES = getAllCountries().map(country => country.cca2);
+
+    getCountryName(country, optionalTranslation) {
+        const translation = optionalTranslation || this.props.translation || 'eng'
+        return country.name[translation] || country.name.common
+    }
+
+
+    handleFilterChange = value => {
+        console.log('values=======', value);
+        const filteredCountries =
+            value === '' ? cca2List : this.fuse.search(value)
+
+        // this._listView.scrollTo({ y: 0 })
+        console.log('filtered countries --------', filteredCountries);
+        this.setState({
+            filter: value,
+            dataValue: filteredCountries
+        })
+    }
+    FlatListItemSeparator = () => {
         return (
-            <View style = {styles.shadow}>
-                <CountryPicker 
-                    countryList={ALL_COUNTRIES}
-                    onChange={value => {
-                        this.setState({ cca2: value.cca2, callingCode: value.callingCode })
-                    }}
-                    cca2={this.state.cca2}
-                    callingCode={this.state.callingCode}
-                    translation="eng"
-                    showCallingCode= {true}
-                    filterable={true}
-                    filterPlaceholder="Search"
-                />
+            <View
+                style={{
+                    height: 1,
+                    width: "100%",
+                    backgroundColor: "#607D8B",
+                }}
+            />
+        );
+    }
+    _keyExtractor = (item, index) => item;
+
+    _renderItem = ({ item }) => (
+        <MyListItem
+            id={item.id}
+            onPressItem={this._onPressItem}
+            selected={!!this.state.selected.get(item.id)}
+            title={item.title}
+        />
+    );
+    render() {
+        return (
+            <View>
+                <TouchableWithoutFeedback onPress={() => { this.toggleModal() }}>
+                    <View
+                        style={styles.touchFlag}
+                    >
+                        <Text style={styles.countryCode}>+{countries[this.state.cca2].callingCode} <FontAwesome name="sort-down" size={15} style={{ textAlign: 'auto' }} /> </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <Modal
+                    style={{ marginTop: "5%", marginLeft: "9%", marginRight: "9%", marginBottom: "5%", backgroundColor: "white" }}
+                    transparent={true}
+                    onBackdropPress={() => console.log("Pressed")}
+                    animationType={'none'}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => this.setState({ modalVisible: false })}   >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.header}>
+                            <FontAwesome name="search" size={18} style={{}} />
+                            <TextInput
+                                autoCorrect={false}
+                                placeholder={'Search...'}
+                                placeholderTextColor={'rgb(200,200,200)'}
+                                style={[
+                                    styles.input,
+                                    styles.inputOnly
+                                ]}
+                                onChangeText={this.handleFilterChange}
+                                value={this.state.filter}
+                            />
+                        </View>
+                        <View style={styles.contentContainer}>
+                            <FlatList
+                                data={this.state.dataValue}
+                                renderItem={({ item }) => (
+                                    <TouchableOpacity onPress={() => { this.onSelectCountry(item) }}>
+                                        <View style={styles.scrollItems}>
+                                            <Image
+                                                style={[styles.imgStyle]}
+                                                source={{ uri: countries[item].flag }}
+                                                keyExtractor={this._keyExtractor}
+                                            />
+                                            <Text style={{ marginLeft: 25, marginTop: 18, fontSize: 15, color: '#8D8E8E' }}>
+                                                {countries[item].name.common}
+                                            </Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                )} />
+                        </View>
+
+                    </View>
+                </Modal>
+
             </View>
-        )
+        );
     }
 }
 
-export default CountrySelector
+export default CountrySelector;
--- app/screen/Onboard/CountrySelector/styles.js
+++ app/screen/Onboard/CountrySelector/styles.js
@@ -1,11 +1,139 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform, PixelRatio } from 'react-native';
+import { getHeightPercent } from './ratio'
 
 const styles = StyleSheet.create({
     shadow: {
-        height:"100%",  
-        width:"20%",
-        backgroundColor: 'rgba(211,211,211,0.3)' 
+        height: "100%",
+        width: "20%",
+        backgroundColor: 'rgba(211,211,211,0.3)'
+    },
+    touchFlag: {
+        backgroundColor: 'rgba(211,211,211,0.3)'
+    },
+    countryCode: {
+
+    },
+    modalContainer: {
+        backgroundColor: 'white',
+        flex: 1
+    },
+    header: {
+        marginTop: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomColor: '#71BFB4',
+        borderBottomWidth: 1.5,
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 0
+    },
+    input: {
+        marginTop:3, 
+        width: '100%'
+    },
+    inputOnly: {
+        marginLeft: 3,
+        fontSize: 21
+    },
+    touchFlag: {
+        height: 19,
+
+    },
+    countryCode: {
+        marginLeft: 10,
+        paddingBottom: 10,
+        textAlignVertical: 'center',
+
+    },
+    downarrow: {
+        paddingTop: 10
+    },
+    contentContainer: {
+        justifyContent: 'center',
+        flex: 1,
+        margin: 10
+
+    },
+    scrollItems: {
+        flexDirection:'row',
+    },
+    imgStyle: {
+        resizeMode: 'contain',
+        width: 26,
+        height: 26,
+        marginLeft: 20,
+        opacity: 0.8, 
+        marginTop:18, 
+        marginBottom: 4
+        
+    },
+    
+    sortDown: {
+        marginBottom: 5
+    },
+    emojiFlag: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: 30,
+        width: 30,
+        height: 30,
+        borderWidth: 1 / PixelRatio.get(),
+        borderColor: 'transparent',
+        backgroundColor: 'transparent'
+    },
+    itemCountry: {
+        flexDirection: 'row',
+        height: getHeightPercent(7),
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    itemCountryFlag: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '7%',
+        width: '15%'
+    },
+    itemCountryName: {
+        justifyContent: 'center',
+        width: '70%',
+        borderBottomWidth: 2 / PixelRatio.get(),
+        borderBottomColor: '#ccc',
+        height: 20
+    },
+    countryName: {
+        fontSize: getHeightPercent(2.2)
+    },
+    scrollView: {
+        flex: 1
+    },
+    letters: {
+        marginRight: 10,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    letter: {
+        height: 25,
+        width: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    letterText: {
+        textAlign: 'center',
+        fontSize: getHeightPercent(2.2)
+    },
+    closeButton: {
+        height: 48,
+        width: '15%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    closeButtonImage: {
+        height: 24,
+        width: 24,
+        resizeMode: 'contain'
     }
+
 })
 
 export default styles;
