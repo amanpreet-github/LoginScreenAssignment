96286ea2d252e9b26272fb66f5e367d4196ccbdd
@amanpreet <amanpreet@codalien.com>
2018-05-09 12:15:23 +0530

    removed extra dependencies

--- app/screen/Onboard/LoginSubsection/loginSubsection.js
+++ app/screen/Onboard/LoginSubsection/loginSubsection.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput, KeyboardAvoidingView } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import { Button, Toast } from 'native-base'
 import styles from './styles'
 import CountrySelector from '@screen/Onboard/CountrySelector'
